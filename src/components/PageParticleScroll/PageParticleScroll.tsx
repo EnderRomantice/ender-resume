3e732ec47125c1cb5a761bc5389aeffacf9e9c7d
EnderRomantice <enderromantic@gmail.com>
2026-07-24 16:26:03 +0800

    fix page scaling and scrolling regression

--- src/components/PageParticleScroll/PageParticleScroll.tsx
+++ src/components/PageParticleScroll/PageParticleScroll.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import type { ReactNode } from "react";
-import { ParticleScroll } from "@/components/canvasui/ParticleScroll";
 import styles from "./PageParticleScroll.module.css";
 
 interface PageParticleScrollProps {
@@ -9,23 +6,5 @@ interface PageParticleScrollProps {
 }
 
 export default function PageParticleScroll({ children }: PageParticleScrollProps) {
-  return (
-    <ParticleScroll
-      className={styles.viewport}
-      point={0.72}
-      band={360}
-      density={2}
-      size={1.15}
-      spread={150}
-      gravity={0.22}
-      drift={0.35}
-      swirl={36}
-      stagger={0.52}
-      fade={0.72}
-      settle={0.85}
-      smoothing={0.32}
-    >
-      {children}
-    </ParticleScroll>
-  );
+  return <div className={styles.viewport}>{children}</div>;
 }
